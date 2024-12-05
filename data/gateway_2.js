9378afeb611a021be2670cf9bff6e49a4a46b5e3
TheYceman <101130438+TheYceman@users.noreply.github.com>
2024-12-05 11:15:05 +0100

    Initial commit

---
+++ data/gateway_2.js
@@ -0,0 +1,1 @@
+var json_gateway_2 = {"type":"FeatureCollection","name":"gateway_2","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"nombre":"Gateway"},"geometry":{"type":"Point","coordinates":[-4.081373611922176,38.859555652848989]}}]}
---
+++ data/tomamuestras_3.js
@@ -0,0 +1,1 @@
+var json_tomamuestras_3 = {"type":"FeatureCollection","name":"tomamuestras_3","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"nombre":"Tomamuestras","decimal":45874.0},"geometry":{"type":"Point","coordinates":[-4.081838255285102,38.862226567409792]}}]}
